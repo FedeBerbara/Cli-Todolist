a675435767f85b005238ca13576fd57edb783721
Federico Berbara <federicomberbara@gmail.com>
2021-03-13 14:26:30 -0300

    Some changes

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 require('colors');
-const { showMenu, pause } = require('./helpers/messages');
+const {
+    inquirerMenu, 
+    pause, 
+    readInput, 
+} = require('./helpers/inquirer');
+const { saveInfo } = require('./helpers/saveFiles');
+const Tasks = require('./models/tasks');
 
 const main = async () => {
-    console.log('Hola Mundo');
 
     let opt = '';
+    const tasks = new Tasks();
 
     do {
-        opt = await showMenu();
-        console.log({opt})
+        // This function print the menu in the console
+        opt = await inquirerMenu();
+        
+        switch (opt) {
+            case '1':
+                // Create a Task
+                const taskData = await readInput('Description:');
+                tasks.createTask(taskData)    
+            break;
+
+            case '2':
+                // List all Tasks
+                console.log(tasks.arrayList);
+            break;
+
+        }
+
+        saveInfo(tasks.arrayList);
+
         await pause();
 
     } while (opt !== '0')
